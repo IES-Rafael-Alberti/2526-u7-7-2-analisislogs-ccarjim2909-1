c418885b1658815ebb4f325c778044372a80fd80
ccarjim2909 <ccarjim2909@g.educaand.es>
2026-04-10 13:48:29 +0200

    terminado en teoria

--- src/main/kotlin/MostrarArchivo.kt
+++ src/main/kotlin/MostrarArchivo.kt
@@ -16,18 +16,33 @@ class MostrarArchivo {
     }
 
     private fun generarInforme(procesador: ProcesadorLog, soloStats: Boolean): String {
+
+        val niveles = procesador.niveles
+
+        val infoLinea = if (niveles == null || "INFO" in niveles) {
+            "INFO: ${procesador.totalInfo}\n"
+        } else ""
+
+        val warningLinea = if (niveles == null || "WARNING" in niveles) {
+            "WARNING: ${procesador.totalWarning}\n"
+        } else ""
+
+        val errorLinea = if (niveles == null || "ERROR" in niveles) {
+            "ERROR: ${procesador.totalError}\n"
+        } else ""
+
         return if (soloStats) {
             "Total lineas: ${procesador.totalLineas}\n" +
-                    "INFO: ${procesador.totalInfo}\n" +
-                    "WARNING: ${procesador.totalWarning}\n" +
-                    "ERROR: ${procesador.totalError}"
+                    infoLinea +
+                    warningLinea +
+                    errorLinea
         } else {
             "Total lineas: ${procesador.totalLineas}\n" +
                     "Lineas validas: ${procesador.lineasValidas}\n" +
                     "Lineas invalidas: ${procesador.lineasInvalidas}\n" +
-                    "INFO: ${procesador.totalInfo}\n" +
-                    "WARNING: ${procesador.totalWarning}\n" +
-                    "ERROR: ${procesador.totalError}\n" +
+                    infoLinea +
+                    warningLinea +
+                    errorLinea +
                     "Primera fecha: ${procesador.primeraFecha}\n" +
                     "Ultima fecha: ${procesador.ultimaFecha}"
         }
